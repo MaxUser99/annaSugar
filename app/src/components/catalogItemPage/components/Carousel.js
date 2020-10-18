513d579ed0c570b8bcbbd36c2289c417db809853
MaxUser99 <maks.user99@gmail.com>
2020-10-18 17:07:21 +0300

    finish carousel

--- app/src/components/catalogItemPage/components/Carousel.js
+++ app/src/components/catalogItemPage/components/Carousel.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Container from '../../container/container';
+import arrowLeft from '../../../assets/icons/arrow-left.svg';
+import arrowRight from '../../../assets/icons/arrow-right.svg';
 
 const MAX_ITEMS = 3;
 
 const Carousel = ({ images }) => {
   const [ previewItem, setPreviewItem ] = useState(images[0]);
   const [ offset, setOffset ] = useState(0);
-  const [ imageLoaded, setLoaded ] = useState(false);
 
   useEffect(() => {
     if (!previewItem) setPreviewItem(images[0]);
@@ -15,17 +16,18 @@ const Carousel = ({ images }) => {
 
   const prevButtonClickHandler = () => setOffset(prev => (prev === 0 ? 0 : prev - 1));
   const nextButtonClickHandler = () => setOffset(prev => (prev + MAX_ITEMS === images.length ? prev : prev + 1));
-  const imageLoadHandler = () => setLoaded(true);
 
   const disableButtons = images.length <= MAX_ITEMS;
 
   return (
     <Container direction='column'>
-      <ImageWrapper $loaded={imageLoaded}>
-        {previewItem && <img src={`/${previewItem}`} onLoad={imageLoadHandler} />}
+      <ImageWrapper justifyContent='center' alignItems='center'>
+        {previewItem && <img src={`/${previewItem}`} />}
       </ImageWrapper>
       <PreviewImagesContainer justifyContent='space-between' alignItems='center' fullWidth>
-        <Button onClick={prevButtonClickHandler} disabled={disableButtons}>P</Button>
+        <Button onClick={prevButtonClickHandler} disabled={disableButtons}>
+          <img src={arrowLeft} alt='' />
+        </Button>
         {
           images.slice(offset, MAX_ITEMS + offset).map(image => (
             <SmallImage
@@ -36,20 +38,21 @@ const Carousel = ({ images }) => {
             />
           ))
         }
-        <Button onClick={nextButtonClickHandler} disabled={disableButtons}>N</Button>
+        <Button onClick={nextButtonClickHandler} disabled={disableButtons}>
+          <img src={arrowRight} alt='' />
+        </Button>
       </PreviewImagesContainer>
     </Container>
   );
 }
 
 const ImageWrapper = styled(Container)`
-  max-width: 390px;
-  max-height: 390px;
-  ${
-    ({ $loaded }) => !$loaded && `
-      width: 350px;
-      height: 350px;
-    `
+  width: 390px;
+  height: 390px;
+  background-color: ${({ theme }) => theme.color.darkBeige};
+  & > img {
+    width: 100%;
+    height: 100%;
   }
 `;
 
@@ -73,6 +76,15 @@ const SmallImage = styled.img`
 
 const PreviewImagesContainer = styled(Container)`
   margin-top: 54px;
+  & > img {
+    cursor: pointer;
+    border: 1px solid transparent;
+    transition: 0.3s;
+    border-radius: 2px;
+    &:hover {
+      border-color: black;
+    }
+  }
 `;
 
 export default Carousel;
