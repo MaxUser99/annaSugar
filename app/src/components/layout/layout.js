882eecfb180815cd0d42007e9d4dfd9a47a9ad39
MaxUser99 <maks.user99@gmail.com>
2020-10-18 16:58:39 +0300

    adjust page layout

--- app/src/components/layout/layout.js
+++ app/src/components/layout/layout.js
@@ -1,24 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Header from '../header/header';
 import Footer from '../footer/footer';
 import Container from '../container/container';
 import ImageModal from '../ImageModal/imageModal';
 
-const Layout = ({ className, children }) => (
-  <StyledContainer className={className} direction='column'>
-    <ImageModal />
-    <Header />
-      { children}
-    <Footer />
-  </StyledContainer>
-);
+function useFooterHeight() {
+  const [ height, setHeight ] = useState();
+
+  useEffect(() => {
+    function calcHeight() {
+      const footer = document.getElementById('footer');
+      if (!footer) return;
+      const { height: footerHeight } = footer.getBoundingClientRect();
+      setHeight(footerHeight);
+    }
+
+    calcHeight();
+
+    window.addEventListener('resize', calcHeight);
+    return () => {
+      window.removeEventListener('resize', calcHeight);
+    }
+  }, []);
+
+  return height;
+}
+
+const Layout = ({ className, children }) => {
+  const footerHeight = useFooterHeight();
+
+  return (
+    <StyledContainer $footerHeight={footerHeight} className={className} direction='column'>
+      <ImageModal />
+      <Header />
+        { children}
+      <Footer />
+    </StyledContainer>
+  );
+}
 
 const StyledContainer = styled(Container)`
   min-height: 100vh;
   width: 100%;
   background-color: ${({ theme }) => theme.color.beige};
-  padding-bottom: 385px;
+  // padding-bottom: 385px;
+  padding-bottom: ${({ $footerHeight }) => $footerHeight}px;
   position: relative;
   box-sizing: border-box;
 `;
