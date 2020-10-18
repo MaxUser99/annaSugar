7af31b7b5bda6647b34cff4bd7faa84c47ea6789
MaxUser99 <maks.user99@gmail.com>
2020-10-18 13:29:29 +0300

    create beads catalog page

--- app/src/pages/catalog/beads.js
+++ app/src/pages/catalog/beads.js
@@ -1,16 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
 import { connect } from 'react-redux';
-import Layout from '../../components/layout/faqLayout';
-import { catalogLinks } from '../../constants/links';
+import { Router } from '@reach/router';
+import { loadBeads, setReviewBead, loadReviewBead } from '../../store/content/catalogActions';
+import CatalogItems from '../../components/catalogItems/catalogItems';
+import ItemPage from '../../components/catalogItems/itemPage';
 
-const Beads = ({ data }) => (
-  <Layout title='Каталог' tabs={catalogLinks}>
-  </Layout>
-);
+const Beads = ({
+  beads,
+  page,
+  reviewItem,
+  loadBeads,
+  loadReviewBead,
+  clearHandler
+}) => {
+  useEffect(() => {
+    if (page === null) loadBeads(0);
+  }, []);
+
+  return (
+    <StyledRouter basepath='/catalog/beads'>
+      <CatalogItems path='/' onItemClick={setReviewBead} items={beads} />
+      <ItemPage
+        path='/:itemId'
+        clearHandler={clearHandler}
+        reviewItem={reviewItem}
+        loadItem={loadReviewBead}
+      />
+    </StyledRouter>
+  );
+};
+
+const StyledRouter = styled(Router)`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
 
 export default connect(
-  ({ content: { beads: { data }}}) => ({
-    beads: data
+  ({ content: { beads: { reviewItem, page, data }}}) => ({
+    beads: data,
+    reviewItem,
+    page
   }),
-  null
+  dispatch => ({
+    loadBeads: page => dispatch(loadBeads(page)),
+    loadReviewBead: id => dispatch(loadReviewBead(id)),
+    clearHandler: () => dispatch(setReviewBead(null))
+  })
 )(Beads);
